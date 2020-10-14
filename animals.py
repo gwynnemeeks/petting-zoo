fdc120907072e06f3ed003ce247070f00687ce45
Gwynne Meeks <meeks.gwynne@gmail.com>
2020-10-14 15:48:17 -0500

    packages added and complete

--- animals.py
+++ animals.py
@@ -1,136 +1,41 @@
+from anaconda import Anaconda
+from cottonmouth import Cottonmouth
+from copperhead import Copperhead
 from datetime import date
-
-class Donkey():
-    def __init__(self, name, species):
-        self.name = name
-        self.species = species
-        self.date_added = date.today()
-        self.walking = True
-
-donkey = Donkey('Astrix', 'donkey')
-
-class Goat():
-    def __init__(self, name, species):
-        self.name = name
-        self.species = species
-        self.date_added = date.today()
-        self.walking = True
-
-goat = Goat('Baby', 'goat')
-
-class Llama():
-    def __init__(self, name, species):
-        self.name = name
-        self.species = species
-        self.date_added = date.today()
-        self.walking = True
-
-llama = Llama('Henrietta', 'llama')
-
-class Cat():
-    def __init__(self, name, species):
-        self.name = name
-        self.species = species
-        self.date_added = date.today()
-        self.walking = True
-
-cat = Cat('Cupcake', cat)
-
-class Dog():
-    def __init__(self, name, species):
-        self.name = name
-        self.species = species
-        self.date_added = date.today()
-        self.walking = True
-
-dog = Dog('Zoey', dog)
-
-class Copperhead():
-    def __init__(self, name, species):
-        self.name = name
-        self.species = species
-        self.date_added = date.today()
-        self.slithering = True
-
-copperhead = Copperhead('Wulfgang', copperhead)
-
-class Python():
-    def __init__(self, name, species):
-        self.name = name
-        self.species = species
-        self.date_added = date.today()
-        self.slithering = True
-
-python = Python('John', python)
-
-class Cottonmouth():
-    def __init__(self, name, species):
-        self.name = name
-        self.species = species
-        self.date_added = date.today()
-        self.slithering = True
-
-cottonmouth = Cottonmouth('Emelda', cottonmouth)
-
-class Kingsnake():
-    def __init__(self, name, species):
-        self.name = name
-        self.species = species
-        self.date_added = date.today()
-        self.slithering = True
-
-kingsnake = Kingsnake('Youngdo', kingsnake)
-
-class Anaconda():
-    def __init__(self, name, species):
-        self.name = name
-        self.species = species
-        self.date_added = date.today()
-        self.slithering = True
+from dog import Dog
+from donkey import Donkey
+from goat import Goat
+from goldfish import Goldfish
+from kingsnake import Kingsnake
+from kitten import Kitten
+from koi import Koi
+from llama import Llama
+from goldfish.goldfish import Goldfish
+from goldfish.goldfish import Goldfish
+from mallard import Mallard
+from python import Python
+from shark import Shark
+from swan import Swan
 
 anaconda = Anaconda('Nicki', anaconda)
-
-class Mallard():
-    def __init__(self, name, species):
-        self.name = name
-        self.species = species
-        self.date_added = date.today()
-        self.swimming = True
-
-mallard = Mallard('Minnie', mallard)
-
-class Goldfish():
-    def __init__(self, name, species):
-        self.name = name
-        self.species = species
-        self.date_added = date.today()
-        self.swimming = True
-
+cottonmouth = Cottonmouth('Emelda', cottonmouth)
+copperhead = Copperhead('Wulfgang', copperhead)
+dog = Dog('Zoey', dog)
+donkey = Donkey('Astrix', 'donkey')
+goat = Goat('Baby', 'goat')
 goldfish = Goldfish('Goldie', goldfish)
-
-class Swan():
-    def __init__(self, name, species):
-        self.name = name
-        self.species = species
-        self.date_added = date.today()
-        self.swimming = True
-
+kingsnake = Kingsnake('Youngdo', kingsnake)
+kitten = Kitten('Cupcake', kitten)
+koi = Koi('Sesshoumaru', koi)
+llama = Llama('Henrietta', 'llama')
+mallard = Mallard('Minnie', mallard)
+python = Python('John', python)
+shark = Shark('Kaiju', shark)
 swan = Swan('Odette', swan)
 
-class Koi():
-    def __init__(self, name, species):
-        self.name = name
-        self.species = species
-        self.date_added = date.today()
-        self.swimming = True
 
-koi = Koi('Sesshoumaru', koi)
 
-class Shark():
-    def __init__(self, name, species):
-        self.name = name
-        self.species = species
-        self.date_added = date.today()
-        self.swimming = True
 
-shark = Shark('Kaiju', shark)
+
+
+
---
+++ kingsnake/kingsnake.py
@@ -0,0 +1,6 @@
+class Kingsnake():
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.slithering = True
