c78d5e9222ef1d346218d0ab491323bc3a817832
Gwynne Meeks <meeks.gwynne@gmail.com>
2020-10-19 13:54:52 -0500

    getters and setters working

--- attractions/snakePit.py
+++ attractions/snakePit.py
@@ -8,7 +8,18 @@ class SnakePit:
     def add(self, animal):
         self.animals.append(animal)
 
-    def printReport(self):
-        print(f'{self.name} is where you will find {self.description} of all sizes including')
+    # def printReport(self):
+    #     print(f'{self.name} is where you will find {self.description} of all sizes including')
+    #     for animal in self.animals:
+    #         print(f'* {animal.name} the {animal.species}')
+
+    def __str__(self):
+        attraction_animals = []
         for animal in self.animals:
-            print(f'* {animal.name} the {animal.species}')
+            attraction_animals.append(f" * {animal.name}")
+        animals_list = ('\n'.join(map(str, attraction_animals)))
+        return f"{self.name} is where you'll find {self.description} like \n{animals_list}"
+
+    @property
+    def last_critter_added(self):
+        return f"{self.animals[-1].name} the {self.animals[-1].species}"
