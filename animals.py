9ea5d7fd7ce2b8df2e2d15f055de5e89d0e56e46
Gwynne Meeks <meeks.gwynne@gmail.com>
2020-10-19 11:47:52 -0500

    relationships established

--- animals.py
+++ animals.py
@@ -15,11 +15,44 @@ from mallard import Mallard
 from python import Python
 from shark import Shark
 from swan import Swan
+from attractions import PettingZoo, SnakePit, Wetlands
 
+varmint_village = PettingZoo("Varmint Village")
+slither_inn = SnakePit("Slither Inn")
+fish_pond = Wetlands("Fish Pond")
 
+anaconda = Anaconda('Nicki', 'anaconda', 'slither chips')
+copperhead = Copperhead('Wulfgang', 'copperhead', 'slither chips')
+cottonmouth = Cottonmouth('Emelda', 'cottonmouth', 'mice')
+dog = Dog('Zoey', 'dog', 'afternoon','puppy snacks')
+donkey = Donkey('Astrix', 'donkey', 'evening', 'Mule chips')
+goat = Goat('Baby', 'goat', 'morning', 'goat snacks')
+goldfish = Goldfish('Goldie', 'goldfish', 'fish flakes')
+kingsnake = Kingsnake('Youngdo', 'kingsnake', 'mice')
+kitten = Kitten('Cupcake', 'kitten', 'morning', 'kitty pebbles')
+koi = Koi('Sesshoumaru', 'koi', 'fish flakes')
+llama = Llama('Henrietta', 'llama', 'afternoon', 'apples, maybe?')
+mallard = Mallard('Minnie', 'mallard', 'duck seed')
+python = Python('John', 'python', 'rats')
+shark = Shark('Kaiju', 'shark', 'chum')
+swan = Swan('Odette', 'swan', 'swan seeds')
 
+varmint_village.add(dog)
+varmint_village.add(donkey)
+varmint_village.add(goat)
+varmint_village.add(kitten)
+varmint_village.add(llama)
+slither_inn.add(anaconda)
+slither_inn.add(copperhead)
+slither_inn.add(cottonmouth)
+slither_inn.add(kingsnake)
+slither_inn.add(python)
+fish_pond.add(goldfish)
+fish_pond.add(koi)
+fish_pond.add(mallard)
+fish_pond.add(shark)
+fish_pond.add(swan)
 
-
-
-
-
+varmint_village.printReport()
+slither_inn.printReport()
+fish_pond.printReport()
---
+++ attractions/wetlands.py
@@ -0,0 +1,14 @@
+class Wetlands:
+    
+    def __init__(self, name):
+        self.name = name
+        self.description = "beautiful water creatures to admire"
+        self.animals = list()
+
+    def add(self, animal):
+        self.animals.append(animal)
+
+    def printReport(self):
+        print(f'{self.name} is where you will find {self.description} of all sizes including')
+        for animal in self.animals:
+            print(f'* {animal.name} the {animal.species}')
